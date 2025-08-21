444751d26fac0dcab0ee35f5cec31773f7a74fd1
Kaustubh22327 <clasherkaustubh@gmail.com>
2025-08-21 13:13:40 +0530

    implemented user authentication middleware

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,10 @@
+export default function errorHandler(err, req, res, _next) {
+    // eslint-disable-next-line no-console
+    console.error('Error:', err);
+    const status = err.status || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(status).json({ error: message });
+  }
+  
+  
+  
